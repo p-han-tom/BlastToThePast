fd9b3d778341669a8523c469e0120bf1fb01b556
JerryLin1 <jerreelynn@gmail.com>
2020-08-03 14:10:32 -0400

    enemy

---
+++ Assets/greenEnemyControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class greenEnemyControl : MonoBehaviour
+{
+    public LayerMask groundLayer;
+    private float speed = 2f;
+    private Transform feetPos;
+    private Transform lookAheadGroundPos;
+    private Transform lookAheadWallPos;
+    private Rigidbody2D rb;
+    private int direction = 1;
+    void Start()
+    {
+        feetPos = transform.Find("Feet");
+        lookAheadGroundPos = transform.Find("LookAheadGround");
+        lookAheadWallPos = transform.Find("LookAheadWall");
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lookAhead();
+        rb.velocity = new Vector2(speed * direction, rb.velocity.y);
+    }
+    void lookAhead()
+    {
+        if (!Physics2D.OverlapCircle(lookAheadGroundPos.position, 0.2f, groundLayer))
+        {
+            turnAround();
+        }
+        if (Physics2D.OverlapCircle(lookAheadWallPos.position, 0.2f, groundLayer))
+        {
+            turnAround();
+        }
+    }
+    public void turnAround()
+    {
+        direction *= -1;
+        transform.eulerAngles = (direction < 0) ? new Vector3(0, 180, 0) : new Vector3(0, 0, 0);
+    }
+}
