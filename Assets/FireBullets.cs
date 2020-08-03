3d551f612d3b60e080f7291ccb4dcc7b30fcb6d7
p-han-tom <hantom0127@gmail.com>
2020-08-03 13:31:09 -0400

    laser gun

--- Assets/FireBullets.cs
+++ Assets/FireBullets.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class FireBullets : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public GameObject bulletPrefab;
+    GameObject bullet;
+    
+
+    public void FireBullet(Vector2 direction) {
+        bullet = Instantiate(bulletPrefab, transform.Find("FirePoint").position, Quaternion.identity);
+        bullet.GetComponent<Rigidbody2D>().AddForce(direction * 2000f);
     }
 }
