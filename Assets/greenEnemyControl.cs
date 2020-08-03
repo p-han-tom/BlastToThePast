1e4f80685db1ded0130c59ca125c2067c8bfe240
JerryLin1 <jerreelynn@gmail.com>
2020-08-03 14:24:55 -0400

    :|

--- Assets/greenEnemyControl.cs
+++ Assets/greenEnemyControl.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class greenEnemyControl : MonoBehaviour
 {
     public LayerMask groundLayer;
+    public LayerMask playerLayer;
     private float speed = 2f;
     private Transform feetPos;
     private Transform lookAheadGroundPos;
     private Transform lookAheadWallPos;
+    private Transform headPos;
     private Rigidbody2D rb;
     private int direction = 1;
     void Start()
@@ -16,6 +18,7 @@ public class greenEnemyControl : MonoBehaviour
         feetPos = transform.Find("Feet");
         lookAheadGroundPos = transform.Find("LookAheadGround");
         lookAheadWallPos = transform.Find("LookAheadWall");
+        headPos = transform.Find("Head");
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -23,6 +26,7 @@ public class greenEnemyControl : MonoBehaviour
     void Update()
     {
         lookAhead();
+        // checkStomp();
         rb.velocity = new Vector2(speed * direction, rb.velocity.y);
     }
     void lookAhead()
@@ -36,9 +40,20 @@ public class greenEnemyControl : MonoBehaviour
             turnAround();
         }
     }
+    // void checkStomp() {
+    //     Debug.Log(headPos);
+    //     if (Physics2D.OverlapCircle(headPos.position, 0.3f, playerLayer)) {
+    //         Destroy (gameObject);
+    //     }
+    // }
     public void turnAround()
     {
         direction *= -1;
         transform.eulerAngles = (direction < 0) ? new Vector3(0, 180, 0) : new Vector3(0, 0, 0);
+    }
+    void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.tag == "Player") {
+            Debug.Log(":(");
+        }
     }
 }
