7c41b84cf6c47d5461f6a00a6d7862c6e549f1ef
JerryLin1 <jerreelynn@gmail.com>
2020-08-03 11:31:13 -0400

    moveable gate + button

---
+++ Assets/buttonControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonControl : MonoBehaviour
+{
+    public Sprite pressedSprite;
+    private Sprite defaultSprite;
+    private bool pressed = false;
+    private SpriteRenderer sr;
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        defaultSprite = sr.sprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "Player") {
+            pressed = true;
+            sr.sprite = pressedSprite;
+        }
+    }
+    void OnTriggerExit2D(Collider2D other) {
+        if (other.gameObject.tag == "Player") {
+            pressed = false;
+            sr.sprite = defaultSprite;
+        }
+    }
+    public bool isPressed() {return pressed;}
+}
---
+++ Assets/gateControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gateControl : MonoBehaviour
+{
+    public GameObject button;
+    public float openSpeed = 0.5f;
+    public float closeSpeed = 0.1f;
+    private buttonControl bc;
+    private Vector3 startPos;
+    private Vector3 endPos;
+    void Start()
+    {
+        bc = button.transform.GetComponent<buttonControl>();
+        startPos = transform.position;
+        endPos = transform.Find("End position").position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position != endPos && bc.isPressed())
+        {
+            transform.position = Vector3.MoveTowards(transform.position, endPos, openSpeed * Time.deltaTime);
+        }
+        else if (transform.position != startPos && !bc.isPressed())
+        {
+            transform.position = Vector3.MoveTowards(transform.position, startPos, closeSpeed * Time.deltaTime);
+        }
+    }
+}
