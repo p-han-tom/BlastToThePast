d583d52e8efe0e145be8d93654fe319f788455c5
p-han-tom <hantom0127@gmail.com>
2020-08-03 17:55:05 -0400

    better gun

--- Assets/FireBullets.cs
+++ Assets/FireBullets.cs
@@ -5,46 +5,52 @@ using UnityEngine;
 public class FireBullets : MonoBehaviour
 {
 
-    public GameObject bulletPrefab;
-    GameObject bullet;
-
     public int reflections;
-    public float maxLength;
-    public LineRenderer lineRenderer;
+    LineRenderer lineRenderer;
     RaycastHit2D hit2D;
-    Ray2D ray;
-
+    bool inWall;
     Transform firepoint;
 
     void Start() {
         firepoint = transform.Find("FirePoint");
+        lineRenderer = transform.root.Find("Line").GetComponent<LineRenderer>();
     }
     
     public void FireBullet(Vector3 mousePos) {
 
+        if (!inWall) {
+            lineRenderer.positionCount = 1;
+            lineRenderer.SetPosition(0, firepoint.position);
+            
 
-        // bullet = Instantiate(bulletPrefab, transform.Find("FirePoint").position, Quaternion.identity);
-        // bullet.GetComponent<Rigidbody2D>().velocity = direction * 50f;
-        lineRenderer.positionCount = 1;
-        lineRenderer.SetPosition(0, firepoint.position);
-        
-
-        Vector3 direction = new Vector3(mousePos.x - firepoint.position.x, mousePos.y - firepoint.position.y, 0);
-        RaycastHit2D rayInfo = Physics2D.Raycast(firepoint.position, direction);
-        
-        for (int i = 0; i < reflections; i ++) {
-            if (rayInfo) {
-                lineRenderer.positionCount++;
-                lineRenderer.SetPosition(lineRenderer.positionCount - 1, rayInfo.point);
-                direction = Vector2.Reflect(direction,rayInfo.normal);
-                rayInfo = Physics2D.Raycast(rayInfo.point, direction);
-            } else {
-                lineRenderer.positionCount ++;
-                lineRenderer.SetPosition(lineRenderer.positionCount-1, direction * 100);
-                
+            Vector3 direction = new Vector3(mousePos.x - firepoint.position.x, mousePos.y - firepoint.position.y, 0);
+            RaycastHit2D rayInfo = Physics2D.Raycast(firepoint.position, direction);
+            
+            for (int i = 0; i < reflections; i ++) {
+                if (rayInfo) {
+                    lineRenderer.positionCount++;
+                    lineRenderer.SetPosition(lineRenderer.positionCount - 1, rayInfo.point);
+                    direction = Vector2.Reflect(direction,rayInfo.normal);
+                    rayInfo = Physics2D.Raycast(new Vector2(rayInfo.point.x + rayInfo.normal.x, rayInfo.point.y + rayInfo.normal.y), direction);
+                } else {
+                    lineRenderer.positionCount ++;
+                    lineRenderer.SetPosition(lineRenderer.positionCount-1, direction * 100);
+                }
             }
         }
-        
+    }
 
+    void OnTriggerEnter2D(Collider2D other) {
+        // Eventually comparetag for wall
+        if (other.gameObject.CompareTag("Mirror")) {
+            inWall = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        // Eventually comparetag for wall
+        if (other.gameObject.CompareTag("Mirror")) {
+            inWall = false;
+        }
     }
 }
