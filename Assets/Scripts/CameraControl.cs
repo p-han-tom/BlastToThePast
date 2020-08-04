d2ac52d8b0dc953fa7443f3e0aab239bf29f5c47
JerryLin1 <jerreelynn@gmail.com>
2020-08-04 12:36:17 -0400

    level

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
-    GameObject player;
-    void Start()
-    {
-        player = GameObject.Find("Player");
-    }
-
     // Update is called once per frame
-    void Update()
+    Transform leader;
+    Vector3 leaderVelocity;
+    Vector3 mousePos;
+    Vector3 leaderPos;
+    float xMaxDist = 1f;
+    float yMaxDist = 1f;
+    float cameraTime = 0.1f;
+    float maxCameraSpeed = 100f;
+    void Start() {
+        leader = GameObject.Find("Player").transform;
+    }
+    void FixedUpdate()
     {
-       transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+        if (leader != null)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, new Vector3(leader.position.x, leader.position.y, transform.position.z), ref leaderVelocity, cameraTime, maxCameraSpeed);
+        }
+    }
+    public IEnumerator cameraShake(float duration, float magnitude)
+    {
+        float timeElapsed = 0f;
+        while (timeElapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            transform.position = new Vector3(transform.position.x + x, transform.position.y + y, transform.position.z);
+
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
     }
 }
