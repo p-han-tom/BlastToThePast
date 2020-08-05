50db184144b5ff3ed73a50a2fcd25726f2780fcd
p-han-tom <hantom0127@gmail.com>
2020-08-05 19:14:48 -0400

    menu

---
+++ Assets/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject enemy;
+    float frequency = 15f;
+    float timer = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = frequency;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > 0) {
+            timer -= Time.deltaTime;
+        } else {
+            Instantiate(enemy, transform.position, Quaternion.identity);
+            timer = frequency;
+        }
+    }
+}
