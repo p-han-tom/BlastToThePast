19fcdfe6bf26295ecea4c747dc9660c5d2555906
JerryLin1 <jerreelynn@gmail.com>
2020-08-05 10:52:02 -0400

    stomp polosu

--- Assets/StompBox.cs
+++ Assets/StompBox.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class StompBox : MonoBehaviour
 {
     private Player player;
+    private Rigidbody2D rb;
     private BoxCollider2D col;
     void Start()
     {
         player = transform.parent.GetComponent<Player>();
+        rb = player.GetComponent<Rigidbody2D>();
         col = GetComponent<BoxCollider2D>();
         col.enabled = false;
     }
     void Update()
     {
-        if (player.getIsGrounded() == false)
+        if (player.getIsGrounded() == false && rb.velocity.y <= 0.1f)
         {
             col.enabled = true;
         }
