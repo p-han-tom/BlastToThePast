43cd0cfbd241e056859439fea687fe6e18658af9
JerryLin1 <jerreelynn@gmail.com>
2020-08-06 10:33:56 -0400

    WEFEPFIJ

---
+++ Assets/BobUpAndDown.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BobUpAndDown : MonoBehaviour
+{
+    private float amplitude = 0.001f;
+    private float frequency = 0.5f;
+
+    Vector3 posOffset = new Vector3();
+    Vector3 tempPos = new Vector3();
+
+    void Update()
+    {
+        posOffset = transform.position;
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+        transform.position = tempPos;
+    }
+}
