6488bd77bcb3eb0e05f849572c0e2304a6ac3415
JerryLin1 <jerreelynn@gmail.com>
2020-08-06 17:48:51 -0400

    FIX

--- Assets/MinistarsControl.cs
+++ Assets/MinistarsControl.cs
@@ -6,10 +6,17 @@ using UnityEngine.UI;
 public class MinistarsControl : MonoBehaviour
 {
     public string level;
+    private Sprite fullMinistar;
     public Sprite emptyMinistar;
     private int stars;
+    void Start() {
+        fullMinistar = transform.GetChild(0).GetComponent<Image>().sprite;   
+    }
     public void UpdateMinistars() {
         stars = PlayerPrefs.GetInt("Level"+level+"Stars", 0);
+        for (int i = 2; i >= 0; i--) {
+            transform.GetChild(i).GetComponent<Image>().sprite = fullMinistar;
+        }
         for (int i = 2; i > stars-1; i--) {
             transform.GetChild(i).GetComponent<Image>().sprite = emptyMinistar;
         }
